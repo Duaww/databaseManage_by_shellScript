b6f530a2c853e3a21d7814676bbb9b46e416b000
tinh <nguyentinh031220@gmail.com>
2020-10-06 16:09:04 +0700

    fix debug

--- database_manage.sh
+++ database_manage.sh
@@ -6,7 +6,7 @@ create_database() {
     echo
     mkdir $database_name
     cd ${database_name}
-    zenity --notification --text="Created database success"
+    zenity --notification --text="Created database success" 
     echo "------------------------------------------"
     echo
 }
@@ -21,8 +21,22 @@ create_table() {
     echo
 
     if [ $option == "1" ]; then
-        echo -n "Enter the name table you want to create :  "
-        read tab_name
+        tab_name=0
+        while true;
+        do
+            echo -n "Enter the name table you want to create :  "
+            read tab_name 
+            mang=$(dir)
+            echo "$mang" >> temp
+            s=$(grep -w "$tab_name" temp)
+            if [ "$s" = "" ];then
+                rm temp
+                break
+            else 
+                zenity --error --text="the name table is dulicated"         
+            fi
+        done
+        
         while true;  
         do
             echo -n "Enter the format (field_name data_type (primary or none) : "
@@ -36,11 +50,11 @@ create_table() {
                 echo "${field}" >> $tab_name
             fi
         done
-        zenity --notification --text="Create table success"
+        zenity --notification --text="Create table success" 
         echo "------------------------------------------"
         echo
     elif [ $option == "2" ]; then
-        zenity --notification --text="Exit"
+        zenity --notification --text="Exit" 
         echo "------------------------------------------"
         echo
     fi
@@ -49,8 +63,23 @@ create_table() {
 
 insert_record() {
 
-    echo -n "Enter the name table you want to insert record : "
-    read tab_name
+   
+    while true;
+    do
+        echo -n "Enter the name table you want to insert record : "
+        read tab_name
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$tab_name" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the name table has not created" 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
+
     tab_name_data=${tab_name}_data
 
     touch $tab_name_data
@@ -74,13 +103,13 @@ insert_record() {
     read -a database
     if [ ${#database[@]} -gt $count_field ]
     then 
-        zenity --error --text="Too much arguments"
+        zenity --error --text="Too much arguments" 
         echo "------------------------------------------"
         echo
         return
     elif [ ${#database[@]} -lt $count_field ]
     then
-        zenity --error --text="Few arguments"
+        zenity --error --text="Few arguments" 
         echo "------------------------------------------"
         echo
         return
@@ -88,7 +117,7 @@ insert_record() {
         for i in ${check_primary}
         do  
             if [ $i == ${database[$primary_key]} ];then
-                zenity --error --text="The primary key is duplicated"
+                zenity --error --text="The primary key is duplicated" 
                 echo "------------------------------------------"
                 echo
                 return
@@ -96,21 +125,49 @@ insert_record() {
         done
     fi
     echo ${database[*]} >> ${tab_name_data}
-    zenity --notification --text="Insert success"
+    zenity --notification --text="Insert success" 
     echo "------------------------------------------"
     echo
 }
 
 update_record() {
-    echo -n "Enter the name table you want to update : "
-    read tab_name
-    echo
+    
+    while true;
+    do
+        echo -n "Enter the name table you want to update record : "
+        read tab_name
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$tab_name" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the name table has not created" 
+            echo -n "the table is existed : " 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
 
     tab_name_data=${tab_name}_data
+    echo "$tab_name_data"
     
-    echo -n "Enter the the value have priamry key : "
-    read primary_key
-    echo
+    
+    while true;
+    do
+        echo -n "Enter the the value have priamry key : "
+        read primary_key
+        mang=$(cat ${tab_name_data})
+        echo "$mang" >> temp
+        s=$(grep -w "$primary_key" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the primary key is not existed" 
+        else 
+            rm temp
+            break
+        fi
+    done
+
 
     record_data=$(grep  "$primary_key" ${tab_name_data})
     if [ -z "record_data" ];
@@ -121,39 +178,64 @@ update_record() {
 
         count_field=$(cat ${tab_name} | cut -d" " -f1|wc -l)
         echo -n "Found the record : $record_data"
+        echo
         echo -n "Change your data for record : "
         read -a database
         echo
 
         if [ ${#database[@]} -gt $count_field ]
         then
-            zenity --error --text="Too much arguments"
+            zenity --error --text="Too much arguments" 
             break
         fi
         if [ ${#database[@]} -lt $count_field ]
         then
-            zenity --error --text="Few arguments"
+            zenity --error --text="Few arguments" 
             break
         fi  
         cat ${tab_name_data} > temp        
         sed s/"${record_data}"/"${database[*]}"/ < temp > ${tab_name_data}
         rm temp
     fi
-    zenity --notification --text="Update table success"
+    zenity --notification --text="Update table success" 
     echo "------------------------------------------"
     echo
 }
 
 delete_record() {
-    echo -n "Enter the name table you want to delete record : "
-    read tab_name
-    echo
+    while true;
+    do
+        echo -n "Enter the name table : "
+        read tab_name
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$tab_name" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the name table has not created" 
+            echo -n "the table is existed : " 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
 
     tab_name_data=${tab_name}_data
 
-    echo -n "Enter the value have primary key : "
-    read primary_key
-    echo
+    while true;
+    do
+        echo -n "Enter the the value have priamry key : "
+        read primary_key
+        mang=$(cat ${tab_name_data})
+        echo "$mang" >> temp
+        s=$(grep -w "$primary_key" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the primary key is not existed" 
+        else 
+            rm temp
+            break
+        fi
+    done
 
 
     zenity --question --text "Are you sure you want to delete ?" 
@@ -162,9 +244,9 @@ delete_record() {
         record_data=$(grep "$primary_key" ${tab_name_data})
         cat ${tab_name_data} > temp
         sed /"${record_data}"/d < temp > $tab_name_data
-        zenity --notification --text="Delete record success"
+        zenity --notification --text="Delete record success" 
     else
-        zenity --notification --text="The record have not been deleted"
+        zenity --notification --text="The record have not been deleted" 
     fi
     echo "------------------------------------------"
     echo
@@ -172,9 +254,22 @@ delete_record() {
 }
 
 delete_table() {
-    echo -n "Enter the name table you want to delete : "
-    read tab_name 
-    echo
+    while true;
+    do
+        echo -n "Enter the name table : "
+        read tab_name
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$tab_name" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the name table has not created" 
+            echo -n "the table is existed : " 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
 
     zenity --question --text "Are you sure you want to delete ?" 
     if [[ $? = 0 ]];
@@ -182,27 +277,45 @@ delete_table() {
         tab_name_data=${tab_name}_data
         rm $tab_name
         rm $tab_name_data    
-        zenity --notification --text="Delete table success"
+        zenity --notification --text="Delete table success" 
     else
-        zenity --notification --text="Table have not been deleted"
+        zenity --notification --text="Table have not been deleted" 
     fi
     echo "------------------------------------------"
     echo
 }
 
 delete_database() {
-    echo -n "Enter the name database : "
-    read database
-    echo
+    # echo -n "Enter the name database : "
+    # read database
+    # echo
+    cd ..
+    while true;
+    do
+        echo -n "Enter the name database : "
+        read database
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$database" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the database has not created" 
+            echo -n "the table is existed : " 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
 
     zenity --question --text "Are you sure you want to delete ?" 
     if [[ $? = 0 ]];
     then
-        cd ..
         rm -r $database   
-        zenity --notification --text="Delete database success"
+        zenity --notification --text="Delete database success" 
+        echo "--------------------------------------"
+        echo $(dir)
     else
-        zenity --notification --text="Database have not been deleted"
+        zenity --notification --text="Database have not been deleted" 
     fi
     echo "------------------------------------------"
     echo
@@ -210,9 +323,24 @@ delete_database() {
 }
 
 search_record() {
-    echo -n "Enter the name table : "
-    read tab_name
-    echo
+    
+    while true;
+    do
+        echo -n "Enter the name table you want to search record : "
+        read tab_name
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$tab_name" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the name table has not created" 
+            echo -n "the table is existed : " 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
+    
 
     tab_name_data=${tab_name}_data
 
@@ -221,14 +349,15 @@ search_record() {
     echo
 
     record_data=$(grep "$value" ${tab_name_data})
-    echo $record_data
+    # echo $record_data
 
     echo -e "\n"
     echo -e "==================table data========================"
     header=$(cut -d" " -f1 ${tab_name})
     for i in $header
     do
-        echo -n "  $i         "
+        # echo -n "  $i         "
+        printf "%-25s"  $i
     done
     echo
     echo -e "----------------------------------------------------"
@@ -238,11 +367,13 @@ search_record() {
     for k in $record_data
     do
         if [ $i -lt $((count_field-1)) ]; then
-            echo -n "$k           "
+            # echo -n "$k           "
+            printf "%-25s"  $k
             i=$((i+1))
         else 
             echo
-            echo -n "$k            "
+            # echo -n "$k            "
+            printf "%-25s"  $k
             i=0
         fi
     done
@@ -253,9 +384,22 @@ search_record() {
 }
 
 display_data() {
-    echo -n "Enter the name of name data table : "
-    read tab_name
-    echo 
+    while true;
+    do
+        echo -n "Enter the name table : "
+        read tab_name
+        mang=$(dir)
+        echo "$mang" >> temp
+        s=$(grep -w "$tab_name" temp)
+        if [ "$s" = "" ];then
+            zenity --error --text="the name table has not created" 
+            echo -n "the table is existed : " 
+            echo $mang
+        else 
+            rm temp
+            break
+        fi
+    done
 
     tab_name_data=${tab_name}_data
     echo -e "\n"
@@ -263,7 +407,8 @@ display_data() {
     header=$(cut -d" " -f1 ${tab_name})
     for i in $header
     do
-        echo -n "  $i         "
+        # echo -n "  $i         "
+        printf "%-25s"  $i  
     done
     echo
     echo -e "----------------------------------------------------"
@@ -274,11 +419,13 @@ display_data() {
     for k in $a
     do
         if [ $i -lt $((count_field-1)) ]; then
-            echo -n "$k           "
+            # echo -n "$k           "
+            printf "%-25s"  $k 
             i=$((i+1))
         else 
             echo
-            echo -n "$k            "
+            # echo -n "$k            "
+            printf "%-25s"  $k 
             i=0
         fi
     done
@@ -287,7 +434,8 @@ display_data() {
     echo
 }
 
-main() {
+
+menu() {
     echo "=========Database manage ==========="
     echo "||       1. Create Database       ||"
     echo "||       2. Create Table          ||"
@@ -301,8 +449,12 @@ main() {
     echo "||       10.Exit                  ||"
     echo "===================================="
     echo
+    
+}
+main() {
     while true;
-    do
+    do 
+        menu;
         echo -n "Choose your option : "
         read option
         case "$option" in
@@ -318,7 +470,7 @@ main() {
             "10") break;;
         esac
     done
-    zenity --notification --text="Exit programming"
+    zenity --notification --text="Exit programming" 
     
 
 }
